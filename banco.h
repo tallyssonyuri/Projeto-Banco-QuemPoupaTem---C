e163bdd1893b5ce6c41d0242c1b2a02e38d87d7b
Tallysson Yuri <115179564+tallyssonyuri@users.noreply.github.com>
2023-10-29 04:30:59 +0000

    Foi adionado a lógica da função listar_clientes() que é responsável por informar ao usuário todos os clientes já cadastrados no banco, também houve pequenas alterações referente a função novo_clientes() no que diz respeito ao tipo de conta

--- banco.h
+++ banco.h
@@ -11,7 +11,7 @@ typedef struct {
 typedef struct {
   char nome[50]; /*Nome de até 49 caracteres*/
   char cpf[12]; /*CPF de 11 caracteres*/
-  char conta; /*Conta de 1 caractere (Tipo A ou B)*/
+  char conta[6]; /*Char que irá armazenar contas Plus ou Comu*/
   double saldo; /*Variável saldo que suporta um número de até 64 bits*/
   char senha[11]; /*Senha de até 10 caracteres*/
   int num_operacoes; /*Váriavel para contar quantas operações já foram realizadas por esse cliente*/
@@ -20,11 +20,12 @@ typedef struct {
 
 } Cliente; /*O struct Cliente é reponsável por armazenar as informações nome, cpf, conta, saldo e senha dos clientes cadastrados*/
 
+
 /*As variáveis externas abaixo são necessárias estarem declaradas dessa forma pois nas duas vezes que incluo o arquivo .h em um arquivo.c, uma cópia da variável é criada gerando erros de compilação*/
 
 extern int num_cadastros; /*A variável num_cadastros é responsável por contar a quantidade de cadastros já realizados*/
 
-extern Cliente cadastro[MAX_CADASTROS]; /*A variável cadastro define a quantidade de clientes que podem ser armazenados no banco, onde cada um assumirá uma posição em ordem crescente de 0 a 999*/
+extern Cliente cadastro[MAX_CADASTROS]; /*O array cadastro define a quantidade de clientes que podem ser armazenados no banco, onde cada um assumirá uma posição em ordem crescente de 0 a 999*/
 
 /*Funções*/ 
 
